02a4a4b5b898482a62749b91cfcef41f4002b388
Stanislau Volkau <svolkau@c1r11s4.42warsaw.pl>
2025-09-25 14:36:11 +0200

    add .c file

---
+++ ft_popen.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_popen.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svolkau <gvardovski@icloud.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 12:09:45 by svolkau           #+#    #+#             */
+/*   Updated: 2025/09/25 14:23:50 by svolkau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_popen(char *file, char *gv[], char type)
+{
+	int fd[2], pid;
+
+	if (!gv || !file || (type != 'r' && type != 'w'))
+		return (-1);
+	if (pipe(fd) < 0)
+		return (-1);
+	pid = fork();
+	if (pid < 0)
+		return (-1);
+	if (pid == 0)
+	{
+		if (type == 'r')
+		{
+			dup2(fd[1], 1);
+			close(fd[0]);
+			close(fd[1]);
+		}
+		if (type == 'w')
+		{
+			dup2(fd[0], 0);
+			close(fd[0]);
+			close(fd[1]);
+		}
+		execvp(file, gv);
+	}
+	if (type == 'r')
+	{
+		close(fd[1]);
+		return(fd[0]);
+	}
+	else
+	{
+		close(fd[0]);
+		return(fd[1]);
+	}
+}
